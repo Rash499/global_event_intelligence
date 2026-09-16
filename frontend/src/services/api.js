b7d5ce3a61c61372aef3e835aa0f4957b441afc2
Rash499 <rashmikadilmin499@gmail.com>
2026-09-16 22:54:28 +0530

    feat(api): add country intelligence API service

---
+++ frontend/src/services/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8000/api",
+  timeout: 30000,
+});
+
+export const getLatestEvents = async (limit = 200) => {
+  const response = await api.get(`/events/latest?limit=${limit}`);
+  return response.data;
+};
+
+export const getCountryEvents = async (countryCode) => {
+  const response = await api.get(`/countries/${countryCode}`);
+  return response.data;
+};
+
+export const getEvent = async (eventId) => {
+  const response = await api.get(`/events/${eventId}`);
+  return response.data;
+};
+
+export const getGlobalStatistics = async () => {
+  const response = await api.get("/statistics/global");
+  return response.data;
+};
+
+export const runIngestion = async () => {
+  const response = await api.post("/ingestion/run");
+  return response.data;
+};
