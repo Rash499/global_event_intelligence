770de11ca321edfb9d4205be64867e3ac806bff0
Rash499 <rashmikadilmin499@gmail.com>
2026-09-21 23:54:24 +0530

    Connect weather globe layers

--- frontend/src/components/globe/Globe.jsx
+++ frontend/src/components/globe/Globe.jsx
@@ -4,10 +4,14 @@ import { useCountryPolygons } from "./useCountryPolygons";
 import { useEventPoints } from "./useEventPoints";
 import { useEventRings } from "./useEventRings";
 import { useCameraFocus } from "./useCameraFocus";
+import { useWeatherPoints } from "./useWeatherPoints";
 
 export default function Globe({
+  mode = "events",
   events,
+  weather,
   onSelectEvent,
+  onSelectWeather,
   onSelectCountry,
   selectedCountryCode,
 }) {
@@ -15,15 +19,38 @@ export default function Globe({
   const globeRef = useRef(null);
 
   useGlobeInit(containerRef, globeRef);
+
   useCountryPolygons({
     globeRef,
     events,
+    weather,
+    mode,
     onSelectCountry,
     selectedCountryCode,
   });
-  useEventPoints({ globeRef, events, onSelectEvent });
-  useEventRings({ globeRef, events });
-  useCameraFocus({ globeRef, events, selectedCountryCode });
+
+  useEventPoints({
+    globeRef,
+    events: mode === "events" ? events : [],
+    onSelectEvent,
+  });
+
+  useEventRings({
+    globeRef,
+    events: mode === "events" ? events : [],
+  });
+
+  useWeatherPoints({
+    globeRef,
+    weather: mode === "weather" ? weather : [],
+    onSelectWeather,
+  });
+
+  useCameraFocus({
+    globeRef,
+    events: mode === "events" ? events : [],
+    selectedCountryCode,
+  });
 
   return <div ref={containerRef} className="globe" />;
 }
