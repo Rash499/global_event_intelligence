8d1d15f74cbe53ab3b77ca6639875c312dd8fd60
Rash499 <rashmikadilmin499@gmail.com>
2026-09-26 14:51:30 +0530

    Frontend: provide interaction context to the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { InteractionProvider } from "./components/interactions/InteractionProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <InteractionProvider>
+      <App />
+    </InteractionProvider>
+  </React.StrictMode>
 );
